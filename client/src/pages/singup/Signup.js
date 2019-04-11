88bf6a288f1b80d481d1827692021c50d3359a03
gnasamx <gnasamx>
2019-04-11 23:10:49 +0530

    :feet: Working on react client & changed server folder name

---
+++ client/src/pages/singup/Signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Signup extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <p>Signup</p>
+  }
+}
+
+export default Signup
